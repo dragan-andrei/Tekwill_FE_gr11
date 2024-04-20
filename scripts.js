60335341800e5cc2af7ded80e38cb142fd478a4b
dragan-andrei <andrejdragan2018@gmail.com>
2024-04-20 13:35:54 +0300

    First JS homework

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+let country = "Moldova";
+let continent = "Europe";
+let population = 3.5;
+let isIsland = false;
+
+
+console.log("Country:", country);
+console.log("Continent:", continent);
+console.log("Population (in millions):", population);
+
+console.log("Is that country is situated on an Island?", isIsland ? "Yes" : "No");
+
+const countryDescription = `${country} is located in  ${continent}, the population is ${population} million of people.`;
+
+console.log("Facts About my country:", countryDescription);
